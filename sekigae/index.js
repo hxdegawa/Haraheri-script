b1df5c18112a2fec08585308d005dc679775cf1c
出川　大和 <hxdegawa@gmail.com>
2016-10-04 21:06:29 +0900

    WTF HOW THE FUCK DO I FIX THIS

--- sekigae/index.js
+++ sekigae/index.js
@@ -1,5 +1,10 @@
 $(function(){
   var count = 0;
+  var isResetScene = false;
+  var $btnExport = $("#export-btn");
+  var $btnImport = $("#import-btn");
+  var $btnNo = $("#btn-accept");
+  var $resetButton = $("#reset-btn");
   var $btnStart = $("#start");
   var $nameList = $("#name-add");
   var $changeName = $("#VariableName");
@@ -19,21 +24,90 @@ $(function(){
   });
   
   $addBtn.click(function(){
+    member.push($nameList.val());
     if($nameList.val().trim()){
-      
-      
-      
-      $("<div id='VariableName' class='names'>Hello</div>").appendTo(document.getElementById("leaderboards"));
-      $("#VariableName").text($nameList.val());
-      $("#VariableName").attr("id", count);
-      member.push($nameList.val());
-      console.log(member);
-      count ++;
-      
-    }
+      addMembers($nameList.val());
+    };
   });
   
-  $(".names").click(function(){
-    alert("hmm?");
+  function addMembers(entry){
+    if(isResetScene === false){
+        
+          $("<div id='VariableName' class='names'>Hello</div>").appendTo(document.getElementById("leaderboards"));
+          $("#VariableName").text(entry);
+          $("#VariableName").attr("id", count);
+          $(".names").innerHeight();
+          $(".names").addClass("entries");
+          $nameList.val("");
+          console.log(member);
+          count ++;
+      }else{
+        toggleReset();
+        };
+      };
+  
+  function toggleReset(){
+    $("#btn-accept").toggleClass("active");
+    $("#input-box").toggleClass("active");
+    $("#name-add").toggleClass("active");
+    $("#btn-add").toggleClass("confirm");
+    isResetScene = !isResetScene;
+    console.log(isResetScene);
+    if(isResetScene === true){
+      $nameList.val("Are you sure?");
+      $nameList.prop("disabled", "true");
+      $addBtn.text("NO");
+  }else{
+      $nameList.val("");
+      $nameList.prop("disabled", null);
+      $addBtn.text("ADD");
+    };
+  };
+  
+  $btnNo.click(function(){
+    
+    toggleReset();
+    $(".names").addClass("removed");
+    setTimeout(function(){
+      $(".names").remove();
+    },800);
+    
   });
+  
+  $resetButton.click(function(){
+    toggleReset();
+  });
+  
+  $btnExport.click(function(){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(member));
+    var dlAnchorElem = document.createElement('a');
+    dlAnchorElem.setAttribute("href", dataStr);
+    dlAnchorElem.setAttribute("download", "Members.json");
+    dlAnchorElem.click();
+  });
+ 
+  $btnImport.click(function(){
+    $("#json-import").click();
+  });
+  $('input[type=file]').on('change', function(e) {
+  var reader = new FileReader();
+  var file = e.target.files[0];
+  reader.onload = function() {
+    member = [];
+    member = JSON.parse(reader.result);
+    console.log(member);
+    $(".names").addClass("removed");
+    setTimeout(function(){
+      $(".names").remove();
+    },800);
+    
+    setTimeout(function(){
+      for(var i = 0; i < member.length; i++){
+          addMembers(member[i]);
+      }
+    },800);
+  };
+  reader.readAsText(file);
 });
+});
+
