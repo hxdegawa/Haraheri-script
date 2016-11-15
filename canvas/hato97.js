b5738787b2c837b073da748c4b0768ac0b958774
出川　大和 <hxdegawa@gmail.com>
2016-11-15 16:44:43 +0900

    Add bullet

--- canvas/hato97.js
+++ canvas/hato97.js
@@ -26,13 +26,42 @@ function loop(){
   if(keys[40]){
     player.y ++;
   }
+  if(keys[32] === 1){
+    bullets.push(new Bullet(player.x, player.y, 0, -2));
+    keys[32] ++;
+  }
   ctx.clearRect(0, 0, 600, 400);
   ctx.save();
   ctx.translate(player.x, player.y);
   ctx.drawImage(img, -25, -25, 50, 50);
   ctx.restore();
+  ctx.fillStyle = "#FA0";
+  for(var i = bullets.length - 1;i>=0; i --){
+    var bullet = bullets[i];
+    bullet.update();
+    if(bullet.y < 0){
+      bullets.splice(i,1);
+    }else{
+      ctx.beginPath();
+      ctx.arc(bullet.x, bullet.y, 5, 0,Math.PI * 2);
+      ctx.fill();
+    }
+  }
   requestAnimationFrame(loop);
 }
+class Bullet{
+  constructor(x, y, vx, vy){
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+  }
+  update(){
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+} 
+
 var player = {
   x: 0, 
   y: 0
